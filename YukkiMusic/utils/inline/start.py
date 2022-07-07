1577f5f0a4bbb4cc731138f0256f3528b564dba5
Lehar MuSiC <103201972+Leharmusicbot@users.noreply.github.com>
2022-07-07 20:56:30 +0530

    Update start.py

--- YukkiMusic/utils/inline/start.py
+++ YukkiMusic/utils/inline/start.py
@@ -19,7 +19,7 @@ def start_pannel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_1"],"❰𝘾𝙊𝙈𝙈𝘼𝙉𝘿𝙎❱", url=f"https://telegra.ph/Lehar-Music-Bot-cOmmANds-06-19",
+                text="❰𝘾𝙊𝙈𝙈𝘼𝙉𝘿𝙎❱", url=f"https://telegra.ph/Lehar-Music-Bot-cOmmANds-06-19"),
             ),
             InlineKeyboardButton(
                 text=_["S_B_2"], callback_data="settings_helper"

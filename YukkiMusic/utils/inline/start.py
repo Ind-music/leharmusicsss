a64eaed307300eff8f2b9ea338b6921f2b31704c
Lehar MuSiC <103201972+Leharmusicbot@users.noreply.github.com>
2022-07-08 19:26:13 +0530

    Update start.py

--- YukkiMusic/utils/inline/start.py
+++ YukkiMusic/utils/inline/start.py
@@ -20,9 +20,6 @@ def start_pannel(_):
         [
             InlineKeyboardButton(text="❰𝘾𝙊𝙈𝙈𝘼𝙉𝘿𝙎❱", url=f"https://telegra.ph/Lehar-Music-Bot-cOmmANds-06-19",
             ),
-            InlineKeyboardButton(  
-                text=_["S_B_2"], callback_data="settings_helper"
-            ), 
         ],
         [
             InlineKeyboardButton(text="❰𝙊𝙬𝙣𝙚𝙧❱", url=f"https://t.me/Dm0Ds_kAkaShI"),
